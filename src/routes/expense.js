1d035deba94806ef8d1bfdcd29afc4b5608001f7
user <kupaev.itrum@gmail.com>
2023-01-18 16:51:02 +0300

    fixed routes

--- src/routes/expense.js
+++ src/routes/expense.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { allExpenses, createExpense, removeExpense, editExpense } = require('../controllers/expense-controller');
 const { validationExpenses } = require('../middlewares/expense-validation');
 
-router.get('/allExpenses', allExpenses);
+router.get('/expenses', allExpenses);
 router.post('/expense', validationExpenses, createExpense);
-router.delete('/removeExpense/:id', removeExpense);
-router.put('/editExpense', validationExpenses, editExpense);
+router.delete('/expense/:id', removeExpense);
+router.put('/expense', validationExpenses, editExpense);
 
 module.exports = router;
